50bae30128d47c0c50c57354e7268c836d616a93
MKachi <mkachi@naver.com>
2018-08-15 05:34:33 +0900

    Add Client project protocol & boost.asio type rename

--- Client/Type.h
+++ Client/Type.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <boost/asio.hpp>
+
 typedef wchar_t				wchar;
 typedef unsigned char		uchar;
 typedef unsigned char		byte;
@@ -9,3 +11,9 @@ typedef unsigned int		uint;
 typedef unsigned long		ulong;
 typedef long long			int64;
 typedef unsigned long long	uint64;
+
+typedef boost::asio::io_service				Service;
+typedef boost::asio::ip::tcp::socket		TcpSocket;
+typedef boost::asio::ip::tcp::endpoint		TcpEndPoint;
+typedef boost::asio::ip::tcp::acceptor		TcpAcceptor;
+typedef boost::system::error_code			ErrorCode;
